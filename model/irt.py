ed6341ec341eb9e2d87c0e2553200bae6d9fa9fb
张振虎 <zhangzhenhu1@100tal.com>
2018-04-20 10:06:20 +0800

    dev

--- model/irt.py
+++ model/irt.py
@@ -420,8 +420,8 @@ class MIrt2PL(BaseIrt):
         with basic_model:
             # 我们假设 \theta\sim N(0, 1) ， a \sim lognormal(0, 1) （对数正态分布），b\sim N(0, 1) ， c\sim beta(2, 5)
             # theta = pm.Normal("theta", mu=0, sd=1, shape=(self.user_count, self.k))
-            theta = pm.MvNormal("theta", mu=np.zeros(self.user_count), cov=np.identity(self.user_count),
-                                shape=(self.user_count, self.k))
+            theta = pm.MvNormal("theta", mu=np.zeros(self.k), cov=np.identity(self.k),
+                                shape=(self.user_count,1))
 
             a = pm.Lognormal("a", mu=0, tau=1, shape=(self.k, self.item_count))
             b = pm.Normal("b", mu=0, sd=1, shape=(1, self.item_count))
