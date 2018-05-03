b7301b8e455a02dc6632cb791b69ce69dd4d5ac1
mszmqp <mszmqp@outlook.com>
2018-05-03 13:38:13 +0800

    cf_algorithm

---
+++ reco_accu_pred/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Copyright (c) 2014 Hal.com, Inc. All Rights Reserved
+#
+'''
+Created on 2018年1月27日
+@author:mengshuai@100tal.com
+the model of the algorithm
+'''
+
+from .evaluate import *
+from .data import *
+from .model import *
+from .utils import *
