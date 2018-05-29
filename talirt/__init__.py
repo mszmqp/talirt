02ff0b5fa2c9925c7ad7a34edf5cf43c5d8796f6
张振虎 <zhangzhenhu1@100tal.com>
2018-05-29 17:05:26 +0800

    dev

---
+++ talirt/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Copyright (c) 2014 Hal.com, Inc. All Rights Reserved
+#
+"""
+模块用途描述
+
+Authors: zhangzhenhu(zhangzhenhu1@100tal.com)
+Date:    2018/5/29 17:05
+"""
